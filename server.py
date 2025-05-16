c42897097634a2b346f42ca702ccb155026ad0d6
MJBeltran13 <143980070+MJBeltran13@users.noreply.github.com>
2025-05-16 15:43:56 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from robot_control import app, get_local_ip
+from robot_control import app
+
+# Static Raspberry Pi IP address
+RASPBERRY_PI_IP = '192.168.101.252'
 
 if __name__ == '__main__':
-    local_ip = get_local_ip()
-    print(f"Server running at: http://{local_ip}:5000")
-    print("You can also access it locally at: http://127.0.0.1:5000")
-    # Run the server on all interfaces
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    print(f"Server running at: http://{RASPBERRY_PI_IP}:5000")
+    # Run the server on the Raspberry Pi's IP
+    app.run(host=RASPBERRY_PI_IP, port=5000, debug=True)
