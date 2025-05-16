c1620dfd5b7df0083d5739583d3ee473094d6613
MJBeltran13 <143980070+MJBeltran13@users.noreply.github.com>
2025-05-16 15:45:47 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from robot_control import app
 RASPBERRY_PI_IP = '192.168.101.252'
 
 if __name__ == '__main__':
-    print(f"Server running at: http://{RASPBERRY_PI_IP}:5000")
-    # Run the server on the Raspberry Pi's IP
-    app.run(host=RASPBERRY_PI_IP, port=5000, debug=True)
+    print(f"Access the server at: http://{RASPBERRY_PI_IP}:5000")
+    print("Running server on all interfaces...")
+    # Run the server on all interfaces (0.0.0.0) but display Raspberry Pi IP for access
+    app.run(host='0.0.0.0', port=5000, debug=True)
